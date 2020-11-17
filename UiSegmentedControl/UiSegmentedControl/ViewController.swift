7756e78ac0d5c882cfd2b0d5f9594ff456945f3b
Alexey Gulyaev <ghost-shadows@yandex.ru>
2020-11-17 18:34:54 +0300

    Clean project

---
+++ UiSegmentedControl/UiSegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiSegmentedControl
+//
+//  Created by Алексей Гуляев on 15.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
