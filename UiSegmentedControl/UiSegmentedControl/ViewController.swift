edf24a3a4b07111731b11dd3bc21d42b15f6e297
Alexey Gulyaev <ghost-shadows@yandex.ru>
2020-11-18 16:27:54 +0300

    add logicgs from segmentedControl

--- UiSegmentedControl/UiSegmentedControl/ViewController.swift
+++ UiSegmentedControl/UiSegmentedControl/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//Создадим outlet для наших элементов
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //Поработаем со свойствами надписи
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+    //
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
     }
-
-
+//Создадим IbAction управления действиями в зависимости от переключения между секциями
+    @IBAction func choiceSegment(_ sender: UISegmentedControl) {
+        //так как наша надпись на текущий момент не видна, изменим это
+        label.isHidden = false
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            label.text = "Selected first section of segmets"
+            label.textColor = .red
+        case 1:
+            label.text = "Selected second section of segmets"
+            label.textColor = .blue
+        case 2:
+            label.text = "Selected third section of segmets"
+            label.textColor = .yellow
+        default:
+            print("Something went wrong")
+        }
+    }
 }
 
